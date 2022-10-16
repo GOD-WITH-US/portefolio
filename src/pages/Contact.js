f1408ee3034b1683c80183da692d16f962b13b45
unknown <e.cronenberger@gmail.com>
2022-10-16 10:21:06 +0200

    contactform emailjs OK

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,41 +1,92 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
-import {CopyToClipboard } from 'react-copy-to-clipboard';
-import '../components/styles.scss';
+import React from "react";
+import Navigation from "../components/Navigation";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+import "./_contact.scss";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9ntbaoa",
+        "template_nmq3yrt",
+        form.current,
+        "wGnIdRInWrJPOq451"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contact">
-    <Navigation />
-    <div className="contactContent">
-      <div className="header"></div>
-      <div className="contactBox">
-        <h1>Contactez-moi</h1>
-        <ul>
-          <li>
-            <i className="fas fa-map-marker-alt"></i>
-            <span>Wittisheim</span>
-          </li>
-          <li>
-            <i className="fas fa-mobile-alt"></i>
-            <CopyToClipboard text="0778404737">
-              <span
-                className="clickInput"
-                onClick={() => {
-                  alert("Téléphone copié !");
-                }}
-              >
-                0778404737
-              </span>
-            </CopyToClipboard>
-          </li>
+      <Navigation />
+      <div className="contactContent">
+        <div className="header"> <div className="title">Contact par mail  </div></div>
+       
+        <div className="contactBox">
+          <form ref={form} onSubmit={sendEmail}>
+            {/* <!-- name --> */}
+            <div className="form-group">
+              <label htmlFor="name">Nom ou Entreprise </label>
+              <input
+                type="name"
+                name="name"
+                className="form-control"
+                id="name"
+                placeholder="Votre nom"
+              />
+            </div>
 
-          <li></li>
-          <li></li>
-        </ul>
+            {/* <!-- email --> */}
+            <div className="form-group">
+              <label htmlFor="email">Email </label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                id="email"
+                placeholder="Votre email"
+              />
+            </div>
+
+            {/* <!-- subject --> */}
+            <div className="form-group">
+              <label htmlFor="subject">Objet </label>
+              <input
+                type="text"
+                name="subject"
+                className="form-control"
+                id="subject"
+                placeholder="Objet de votre mail"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="message">Message </label>
+              <textarea
+                name="message"
+                className="form-control"
+                id="email_body"
+                rows="8"
+              ></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Envoyer
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
