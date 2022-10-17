e6e01c6dd94a8f15bc74e33f4697b036e352b5f8
unknown <e.cronenberger@gmail.com>
2022-10-17 18:51:59 +0200

    correction style et bug android

--- src/components/knowledges/Experience.js
+++ src/components/knowledges/Experience.js
@@ -2,31 +2,44 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="experience" >
+    <div className="experience">
       <h3>Expériences</h3>
       <div className="exp-1">
         <h4>Platrier Plaquiste</h4>
         <h5>2003 / 2012</h5>
-        <p>J'ai exercé ce métier pendant 10 ans avec passion. <br/>
-          Il m'a permis de développer et de vivre  une passion <br/> pour les monuments anciens et l'écoconstruction.
-          <br/> J'ai aussi acquis le savoir nécessaire à la construction <br/> de ma maison bioclimatique. 
+        <p>
+          J'ai exercé ce métier pendant 10 ans avec passion. <br />
+          Il m'a permis de développer et de vivre une passion <br /> pour les
+          monuments anciens et l'écoconstruction.
+          <br /> J'ai aussi acquis le savoir nécessaire à la construction <br />{" "}
+          de ma maison bioclimatique.
         </p>
-
       </div>
 
       <div className="exp-2">
         <h4>Magasinier Cariste & Gestion de Stock</h4>
         <h5>2013 / 2021</h5>
-        <p>J'ai exercé ce métier qui m'a fait découvrir le monde automobile en travaillant pour BMW dans l'expédition et la gestion de piéces détachées.
-          J'ai ensuite choisi de me rapprocher de mon lieu de vie et j'ai donc rejoins Gedimat une entreprise de matériaux de construction.
+        <p>
+          J'ai exercé ce métier qui m'a fait découvrir le monde automobile en
+          travaillant pour BMW dans l'expédition et la gestion de piéces
+          détachées. J'ai ensuite choisi de me rapprocher de mon lieu de vie et
+          j'ai donc rejoins Gedimat une entreprise de matériaux de construction.
         </p>
-
       </div>
 
-
+      <div className="exp-3">
+        <h4>Développeur Web et Mobile</h4>
+        <h5>2021/2022</h5>
+        <p>
+          Titre Pro Développeur Web et Mobile niveau V (BAC +2) <br/><br/>
+          Obtenu aprés une formation de 798 heures sur 6 mois<br /><br/>
+          HTML5/CSS3/JavaScript/PHP/PostgreSQL/NodeJS <br /><br/>
+          Spécialisation : React 1 mois <br /><br/>
+          Projet: Essencial reseau d'entraide et de partage autour de l'autonomie et de l'écologie
+        </p>
+      </div>
     </div>
-  )
-
+  );
 };
 
-export default Experience
+export default Experience;
